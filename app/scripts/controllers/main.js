fad8f078be53fbd9659ffef35bb0ee11e7f14131
Sarah Ghidalia <Mileem@macbook-air-de-sarah.home>
2016-04-11 19:40:39 +0200

    First commit yeoman angular generator

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pokedexAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pokedexAngularApp
+ */
+angular.module('pokedexAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
