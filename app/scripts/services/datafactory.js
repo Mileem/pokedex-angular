b8f3b93841f49dbe2f81cb85fcf439d6092776e9
Sarah Ghidalia <Mileem@dy080149.etu.univ-rennes1.fr>
2016-04-12 10:53:23 +0200

    Create factory for get data from api

---
+++ app/scripts/services/datafactory.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name pokedexAngularApp.dataFactory
+ * @description
+ * # dataFactory
+ * Factory in the pokedexAngularApp.
+ */
+angular.module('pokedexAngularApp')
+  .factory('dataFactory', function ($http) {
+    // Service logic
+    // ...
+
+    var urlBase = 'http://pokeapi.co/api/v2';
+    var dataFactory = {};
+
+    dataFactory.getPokemons = function () {
+      return $http.get(urlBase + '/pokemon/?limit=811&offset=0');
+    }
+
+    // Public API here
+    return dataFactory;
+  });
